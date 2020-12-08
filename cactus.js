43275c904b2b2c26cc81accb3406e9cebb089023
Victor Tran <vicr12345@gmail.com>
2020-12-08 22:02:55 +1100

    Fix some tar logic

--- cactus.js
+++ cactus.js
@@ -9,11 +9,12 @@ module.exports = (config) => (/** @type {Express.Request} */ req, /** @type {Exp
     if (fs.existsSync(pkgfile)) {
         //Find out how big the package file is
         const tarLs = spawn('tar', ['-tvO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
-        tarLs.stdout.on('data', (data) => {
-            let size = data.toString().split(" ")[2];
-            res.set('Content-Length', size);
-        });
+        tarLs.stdout.setEncoding("utf-8");
         tarLs.on('close', (code) => {
+            let size = tarLs.stdout.read().toString().split(" ")[2];
+            console.log("size: " + size);
+            res.set('Content-Length', size);
+
             //Send over the package file
             const tar = spawn('tar', ['-xO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
             tar.stdout.pipe(res, {
@@ -23,6 +24,7 @@ module.exports = (config) => (/** @type {Express.Request} */ req, /** @type {Exp
                 res.end();
             });
         });
+    } else {
+        next();
     }
-    next();
 }
