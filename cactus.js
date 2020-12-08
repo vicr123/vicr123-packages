4976143d79c03eea809e063d6bc24c452af67185
Victor Tran <vicr12345@gmail.com>
2020-12-08 22:20:49 +1100

    Hopefully fix tar logic

--- cactus.js
+++ cactus.js
@@ -1,36 +1,28 @@
+const util = require('util');
 const fs = require('fs');
-const { spawn, execSync } = require('child_process');
+const { spawn } = require('child_process');
+const execFile = util.promisify(require('child_process').execFile);
 
 // tar -xO --file=cactus-recovery-media-20201206-1-x86_64.tar opt/cactus-recovery-media/rootfs.squashfs
 
-module.exports = (config) => (/** @type {Express.Request} */ req, /** @type {Express.Response} */ res, next) => {
+module.exports = (config) => async (/** @type {Express.Request} */ req, /** @type {Express.Response} */ res, next) => {
     console.log("Cactus!" + req.params.rootfile);
     let pkgfile = `${config.rootPath}/cactus/${req.params.arch}/cactus-recovery-media-${req.params.rootfile}-1-${req.params.arch}.tar`;
     if (fs.existsSync(pkgfile)) {
         //Find out how big the package file is
-        const tarLs = spawn('tar', ['-tvO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
-        tarLs.stdout.setEncoding("utf-8");
+        let { stdout: tarLs } = await execFile('tar', ['-tvO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
 
-        let chunks = [];
-        tarLs.stdout.on('readable', () => {
-            let chunk;
-            while (null !== (chunk = tarLs.stdout.read())) {
-              chunks.push(chunk);
-            }
+        let size = tarLs.split(" ")[2];
+        console.log("size: " + size);
+        res.set('Content-Length', size);
+
+        //Send over the package file
+        const tar = spawn('tar', ['-xO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
+        tar.stdout.pipe(res, {
+            end: false
         });
-        tarLs.on('close', (code) => {
-            let size = chunks.join("").split(" ")[2];
-            console.log("size: " + size);
-            res.set('Content-Length', size);
-
-            //Send over the package file
-            const tar = spawn('tar', ['-xO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
-            tar.stdout.pipe(res, {
-                end: false
-            });
-            tar.on('close', (code) => {
-                res.end();
-            });
+        tar.on('close', (code) => {
+            res.end();
         });
     } else {
         next();
