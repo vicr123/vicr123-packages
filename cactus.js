32c925b86ed8b88156611bfe46c3c10572890f7c
Victor Tran <vicr12345@gmail.com>
2020-12-08 21:56:09 +1100

    Initial Commit

---
+++ cactus.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const { spawn, execSync } = require('child_process');
+
+// tar -xO --file=cactus-recovery-media-20201206-1-x86_64.tar opt/cactus-recovery-media/rootfs.squashfs
+
+module.exports = (config) => (/** @type {Express.Request} */ req, /** @type {Express.Response} */ res, next) => {
+    console.log("Cactus!" + req.params.rootfile);
+    let pkgfile = `${config.rootPath}/cactus/${req.params.arch}/cactus-recovery-media-${req.params.rootfile}-1-${req.params.arch}.tar`;
+    if (fs.existsSync(pkgfile)) {
+        //Find out how big the package file is
+        const tarLs = spawn('tar', ['-tvO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
+        tar.stdout.on('data', (data) => {
+            let size = data.toString().split(" ")[2];
+            res.set('Content-Length', size);
+        });
+
+        //Send over the package file
+        const tar = spawn('tar', ['-xO', `--file=${pkgfile}`, 'opt/cactus-recovery-media/rootfs.squashfs']);
+        tar.stdout.pipe(res, {
+            end: false
+        });
+        tar.on('close', (code) => {
+            res.end();
+        });
+    }
+    next();
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+const process = require('process');
+const fs = require('fs');
+
+let config = JSON.parse(fs.readFileSync("config.json"));
+
+const cactusDetect = require('./cactus')(config);
+
+let app = express();
+app.use('/cactus/rootfs/:arch/:rootfile.squashfs', cactusDetect);
+app.use('/', express.static(config.rootPath));
+app.use('/', serveIndex(config.rootPath));
+
+let port = config.port;
+if (process.argv.length > 2) {
+    port = parseInt(process.argv[2]);
+}
+app.listen(port, function() {
+    console.log(`Listening on port ${port}`);
+});
